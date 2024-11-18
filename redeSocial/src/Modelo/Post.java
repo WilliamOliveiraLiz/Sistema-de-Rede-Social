7f56c64f047e53c7606d9db4f2ff8371e69b5e9c
william_liz <william_liz@estudante.sesisenai.org.br>
2024-11-18 20:55:55 -0300

    inplementação post completa

--- redeSocial/src/Modelo/Post.java
+++ redeSocial/src/Modelo/Post.java
@@ -1,5 +1,4 @@
 package Modelo;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -8,5 +7,82 @@ public class Post {
     private Usuario autor;
     private String conteudo;
     private LocalDateTime dataPublicaçao;
-    private List
+    private List<Usuario> curtidas;
+    private List<Comentario> comentarios;
+
+    public Post(int ID, Usuario autor, String conteudo, LocalDateTime dataPublicaçao, List<Usuario> curtidas, List<Comentario> comentarios) {
+        this.ID = ID;
+        this.autor = autor;
+        this.conteudo = conteudo;
+        this.dataPublicaçao = dataPublicaçao;
+        this.curtidas = curtidas;
+        this.comentarios = comentarios;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public Usuario getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Usuario autor) {
+        this.autor = autor;
+    }
+
+    public String getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+    }
+
+    public LocalDateTime getDataPublicaçao() {
+        return dataPublicaçao;
+    }
+
+    public void setDataPublicaçao(LocalDateTime dataPublicaçao) {
+        this.dataPublicaçao = dataPublicaçao;
+    }
+
+    public List<Usuario> getCurtidas() {
+        return curtidas;
+    }
+
+    public void setCurtidas(List<Usuario> curtidas) {
+        this.curtidas = curtidas;
+    }
+
+    public List<Comentario> getComentarios() {
+        return comentarios;
+    }
+
+    public void setComentarios(List<Comentario> comentarios) {
+        this.comentarios = comentarios;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "ID=" + ID +
+                ", autor=" + autor +
+                ", conteudo='" + conteudo + '\'' +
+                ", dataPublicaçao=" + dataPublicaçao +
+                ", curtidas=" + curtidas +
+                ", comentarios=" + comentarios +
+                '}';
+    }
+    public void adicionaCurtida(Usuario usuario){curtidas.add(usuario);
+    }
+    public void removerCurtida(Usuario usuario){curtidas.add(usuario);
+    }
+    public void adicionarComentario(Comentario comentario){comentarios.add(comentario);}
+    public void removerComentario(Comentario comentario){comentarios.remove(comentario);}
 }
+
