2f3b94a42259cf144870cb1503005f8214df80ea
william_liz <william_liz@estudante.sesisenai.org.br>
2024-11-18 21:28:26 -0300

    inicio da inplementação da classe GerenciadorPosts

--- redeSocial/src/Gerenciadores/GerenciadorPosts.java
+++ redeSocial/src/Gerenciadores/GerenciadorPosts.java
@@ -1,4 +1,38 @@
 package Gerenciadores;
 
+import Modelo.Post;
+
+import java.util.List;
+
 public class GerenciadorPosts {
+    private List<Post> Posts;
+    private int proximoID;
+    public void criar(Post post){
+
+    }
+    . public Post buscarPorId(int id){
+
+    }
+    public List<Post> listarPorUsuario(int idUsuario){
+
+    }
+    public void curtir(int idPost, int idUsuario){
+
+    }
+    public void descurtir(int idPost, int idUsuario){
+
+    }
+    public void comentar(Comentario comentario){
+
+    }
+    public boolean deletar(int id){
+
+    }
+    private void validarPost(Post post){
+
+    }
+
+
+
+
 }
