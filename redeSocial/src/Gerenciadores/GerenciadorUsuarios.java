19e93a704c41c4317d63302993f7db541297dcdb
WilliamOliveiraLiz <William_liz@estudante.sesisenai.org.br>
2024-11-24 17:16:29 -0300

    GerenciadorUsuarios inplementado falta prencher metodos

--- redeSocial/src/Gerenciadores/GerenciadorUsuarios.java
+++ redeSocial/src/Gerenciadores/GerenciadorUsuarios.java
@@ -1,4 +1,39 @@
 package Gerenciadores;
 
+import Modelo.Usuario;
+
+import java.util.List;
+
 public class GerenciadorUsuarios {
+    private List<Usuario> usuarios;
+    private int proximoID;
+    public void Cadastrar(Usuario usuario){
+
+
+    }
+    public Usuario buscarPorID(int ID){
+
+    }
+    public Usuario buscarPorUsername(String username){
+
+    }
+    public List<Usuario> buscarPorNome(String nome){
+
+    }
+    public boolean atualizar(Usuario usuario){
+
+    }
+    public boolean deletar(int id){
+
+    }
+    public void adicionarAmizade(int idUsuario1, int idUsuario2){
+
+    }
+    public void removerAmizade(int idUsuario1, int idUsuario2){
+
+    }
+    private void validarUsuario(Usuario usuario){
+
+    }
+
 }
